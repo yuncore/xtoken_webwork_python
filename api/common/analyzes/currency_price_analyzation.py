bafbb7354fbcbaad19111a65b477c7d18a54f3d1
divisey <18656007202@163.com>
2018-05-24 16:05:29 +0800

    upgrade currency history price analyze

--- api/common/analyzes/currency_price_analyzation.py
+++ api/common/analyzes/currency_price_analyzation.py
@@ -1,16 +1,14 @@
 from api.db import stat_db, crawl_db, CrawlCollections, StatCollections, relation_db, RelationCollections, DESCENDING, \
     ASCENDING
 from datetime import datetime
-from api.common.utils.util import timestamp_to_datetime
-import time
-import re
-import json
+from api.common.utils.util import timestamp_to_datetime, str_to_timestamp
 import pandas as pd
-import numpy as np
+import time
+import json
 
 
 # 将从coinmarketcap中抓取下来的货币历史价格信息重新加工，生成价格排名
-def history_price_time_resample():
+def history_price_time_resample(ahead_time=0):
     """
     遍历获取到的历史价格数据的货币，整理对应价格的时间戳数据，使之对应为%Y:%m:%s 00:00:00。即将时分秒设置为0.
     将整理后的数据存入stat DB中的history_currency_price集合中。该集合的基本结构为：
@@ -34,143 +32,179 @@ def history_price_time_resample():
     }
     :return:
     """
+    print('start')
     # 遍历所有货币的历史数据，逐个进行数据整理, 取消cursor的超时时间，需要显式关闭cursor
     currency_cursor = crawl_db[CrawlCollections.COINMARKET_HISTORY_PRICE].find(no_cursor_timeout=True)
-    currencies = [{'_id': currency['_id'], 'data': currency['data']} for currency in currency_cursor]
-
-    for currency in currencies:
+    print('connection')
+    for currency in currency_cursor:
         start = time.time()
         currency_id = currency['_id']
+        print('start analyze {0}'.format(currency_id))
+        # 查询上一次数据分析的时间，如果数据库中纪录不存在，则将其设置为0
+        if 'last_resample_time' in currency:
+            last_resample_time = currency['last_resample_time']
+        else:
+            last_resample_time = 0
+
+        # 将json转换为dict
         try:
-            data = json.loads(currency['data'])
+            price_data = json.loads(currency['data'])
         except Exception:
             print('error occurred when convert currency data to json')
             continue
-        market_cap = data['market_cap_by_available_supply']
-        price_usd = data['price_usd']
-        price_btc = data['price_btc']
-        price_time_resample('market_cap', market_cap, currency_id)
-        price_time_resample('price_usd', price_usd, currency_id)
-        price_time_resample('price_btc', price_btc, currency_id)
+
+        # 分析market_cap, price_usd, price_btc的数据，对应的日期数据
+        market_cap = filter_not_resample_data(price_data['market_cap_by_available_supply'], last_resample_time, ahead_time)
+        # print('market_cap length {0}'.format(len(market_cap)))
+        for item in market_cap:
+            # format_date = reformat_timestamp(item[0])
+            stat_db[StatCollections.HISTORY_CURRENCY_PRICE_STAT].find_one_and_update(
+                {'_id': item[0]},
+                {'$set': {'{0}.{1}'.format('market_cap', currency_id): item[1]}}, upsert=True)
+
+        price_usd = filter_not_resample_data(price_data['price_usd'], last_resample_time, ahead_time)
+        for item in price_usd:
+            # format_date = reformat_timestamp(item[0])
+            stat_db[StatCollections.HISTORY_CURRENCY_PRICE_STAT].find_one_and_update(
+                {'_id': item[0]},
+                {'$set': {'{0}.{1}'.format('price_usd', currency_id): item[1]}}, upsert=True)
+
+        price_btc = filter_not_resample_data(price_data['price_btc'], last_resample_time, ahead_time)
+        for item in price_btc:
+            # format_date = reformat_timestamp(item[0])
+            stat_db[StatCollections.HISTORY_CURRENCY_PRICE_STAT].find_one_and_update(
+                {'_id': item[0]},
+                {'$set': {'{0}.{1}'.format('price_btc', currency_id): item[1]}}, upsert=True)
+
+        # 设置新的resample time
+        try:
+            new_resample_time = price_data['market_cap_by_available_supply'][-1][0]
+        except IndexError:
+            new_resample_time = 0
+        crawl_db[CrawlCollections.COINMARKET_HISTORY_PRICE].find_one_and_update(
+            {'_id': currency_id},
+            {'$set': {'last_resample_time': new_resample_time}})
+
         print('resample {0} completed, used {1}'.format(currency_id, time.time() - start))
+
+    # 关闭数据库连接
     currency_cursor.close()
 
+
+def market_cap_total_resample(ahead_time=0):
     # 整理market_cap_total和market_cap_altcoin
     market_cap_total = crawl_db[CrawlCollections.COINMARKET_TOTAL_MARKET_CAP].find_one({'type': 'MARKETCAP_TOTAL'})
 
-    # 保存market cap total数据
+    # 处理market cap total数据
     if market_cap_total is not None:
-        try:
-            json_data = json.loads(market_cap_total['data'])
-            market_cap_data = json_data['market_cap_by_available_supply']
-            volume_usd_data = json_data['volume_usd']
-            price_time_resample('market_cap_total', market_cap_data)
-            price_time_resample('volume_usd_total', volume_usd_data)
-        except Exception:
-            print('error occurred when convert market cap or volume usd to json')
+        json_data = json.loads(market_cap_total['data'])
+        market_cap = json_data['market_cap_by_available_supply']
+        volume_usd = json_data['volume_usd']
+        if 'last_resample_time' in market_cap_total:
+            last_resample_time = market_cap_total['last_resample_time']
+        else:
+            last_resample_time = 0
 
+        # 分析market_cap对应的日期数据
+        market_cap_resample = filter_not_resample_data(market_cap, last_resample_time, ahead_time)
+        for item in market_cap_resample:
+            stat_db[StatCollections.HISTORY_CURRENCY_PRICE_STAT].find_one_and_update(
+                {'_id': item[0]},
+                {'$set': {'market_cap_total': item[1]}}, upsert=True)
+
+        # 分析volume usd 对应的日期数据
+        volume_usd_resample = filter_not_resample_data(volume_usd, last_resample_time, ahead_time)
+        for item in volume_usd_resample:
+            stat_db[StatCollections.HISTORY_CURRENCY_PRICE_STAT].find_one_and_update(
+                {'_id': item[0]},
+                {'$set': {'volume_usd_total': item[1]}}, upsert=True)
+
+        # 设置新的resample time
+        try:
+            new_resample_time = market_cap[-1][0]
+        except IndexError:
+            new_resample_time = 0
+        crawl_db[CrawlCollections.COINMARKET_TOTAL_MARKET_CAP].find_one_and_update(
+            {'type': 'MARKETCAP_TOTAL'},
+            {'$set': {'last_resample_time': new_resample_time}})
+
+
+def market_cap_altcoin_resample(ahead_time=0):
     market_cap_altcoin = crawl_db[CrawlCollections.COINMARKET_TOTAL_MARKET_CAP].find_one({'type': 'MARKETCAP_ALTCOIN'})
 
     # 保存market cap altcoin数据
     if market_cap_altcoin is not None:
+        json_data = json.loads(market_cap_altcoin['data'])
+        market_cap = json_data['market_cap_by_available_supply']
+        volume_usd = json_data['volume_usd']
+        if 'last_resample_time' in market_cap_altcoin:
+            last_resample_time = market_cap_altcoin['last_resample_time']
+        else:
+            last_resample_time = 0
+
+        # 分析market_cap对应的日期数据
+        market_cap_resample = filter_not_resample_data(market_cap, last_resample_time, ahead_time)
+        for item in market_cap_resample:
+            stat_db[StatCollections.HISTORY_CURRENCY_PRICE_STAT].find_one_and_update(
+                {'_id': item[0]},
+                {'$set': {'market_cap_altcoin': item[1]}}, upsert=True)
+
+        # 分析volume usd 对应的日期数据
+        volume_usd_resample = filter_not_resample_data(volume_usd, last_resample_time, ahead_time)
+        for item in volume_usd_resample:
+            stat_db[StatCollections.HISTORY_CURRENCY_PRICE_STAT].find_one_and_update(
+                {'_id': item[0]},
+                {'$set': {'volume_usd_altcoin': item[1]}}, upsert=True)
+
+        # 设置新的resample time
         try:
-            json_data = json.loads(market_cap_altcoin['data'])
-            market_cap_data = json_data['market_cap_by_available_supply']
-            volume_usd_data = json_data['volume_usd']
-            price_time_resample('market_cap_altcoin', market_cap_data)
-            price_time_resample('volume_usd_altcoin', volume_usd_data)
-        except Exception:
-            print('error occurred when convert market cap or volume usd to json')
+            new_resample_time = market_cap[-1][0]
+        except IndexError:
+            new_resample_time = 0
+        crawl_db[CrawlCollections.COINMARKET_TOTAL_MARKET_CAP].find_one_and_update(
+            {'type': 'MARKETCAP_ALTCOIN'},
+            {'$set': {'last_resample_time': new_resample_time}}
+        )
 
 
-def price_time_resample(field_name, data, currency_id=None):
+def filter_not_resample_data(data, last_resample_time, ahead_time=0):
     """
-    整理market cap的时间戳，并数据更新到数据库中
-    :param field_name: 字段名称
-    :param data: market_cap / price_usd / price_btc价格数据
-    :param currency_id: 货币id
+    :param data: 需要过滤的数组数据
+    :param last_resample_time: 上一次数据清洗的时间， 单位是毫秒
+    :param ahead_time: 在上一次数据清洗时间的基础上提前的时间，单位是毫秒
     :return:
     """
-    # 从coinmarketcap中抓取的价格数据是按照时间顺序排列的，反序遍历货币价格数据
-    # 为了节约计算时间，在设置了start的情况下，如果价格数据的时间小于start则跳出循环
-    start = time.time() - 3600 * 24 * 7
-    
-
+    original_data = []
+    if last_resample_time == 0:
+        t = time.time() * 1000 - ahead_time
+    else:
+        t = time.time() * 1000 - ahead_time
     for item in reversed(data):
-        original_date = datetime.fromtimestamp(item[0] / 1000)
-
-        # 将数据转化成格式为%Y-%m-%d字符串，再将字符串转换为时间戳，以达到将时分秒置为0的目的
-        str_time = original_date.strftime('%Y-%m-%d')
-        format_date = datetime.strptime(str_time, '%Y-%m-%d').timestamp()
-
-        # 如果当前计算的时间小于分析的起识时间，跳出循环
-        if start and format_date < start:
-            break
-
-        # 在stat collection中查找该日期的记录
-        price_data = stat_db[StatCollections.HISTORY_CURRENCY_PRICE_STAT].find_one({'_id': format_date})
-
-        # 如果集合名字是'market_cap', 'price_usd', 'price_btc'其中的一个，插入和更新的集合名字写法不一样
-        if field_name in ['market_cap', 'price_usd', 'price_btc']:
-            if price_data is not None:
-                stat_db[StatCollections.HISTORY_CURRENCY_PRICE_STAT].update_one(
-                    {'_id': price_data['_id']},
-                    {'$set': {'{0}.{1}'.format(field_name, currency_id): item[1]}})
-            else:
-                stat_db[StatCollections.HISTORY_CURRENCY_PRICE_STAT].insert_one(
-                    {'_id': format_date, field_name: {'{0}'.format(currency_id): item[1]}})
+        if item[0] > t:
+            original_data.append(item)
         else:
-            # 集合名字是market_cap_total，volume_usd_total, market_cap_altcoin, volume_usd_altcoin
-            if price_data is not None:
-                stat_db[StatCollections.HISTORY_CURRENCY_PRICE_STAT].update_one({'_id': price_data['_id']},
-                                                                                {'$set': {field_name: item[1]}})
-            else:
-                stat_db[StatCollections.HISTORY_CURRENCY_PRICE_STAT].insert_one(
-                    {'_id': format_date, field_name: item[1]})
+            break
+    if len(original_data) > 0:
+        df = pd.DataFrame(original_data, columns=['time', 'price'])
+        df.time = df['time'].apply(lambda x: datetime.fromtimestamp(x / 1000))
+        df.set_index('time', inplace=True)
+        df = df.resample('D').mean()
+        return [[index.timestamp(), float(row['price'])] for index, row in df.iterrows()]
+    else:
+        return []
 
 
-def currency_rank_analyze():
-    """计算每个货币的排名随日期的变化"""
-    try:
-        d = stat_db[StatCollections.HISTORY_CURRENCY_RANK].find_one({'currency_id': 'bitcoin'})
-        latest = max(d['rank_market_cap'], key=lambda x: x[0])
-    except (TypeError, KeyError) as e:
-        latest = [0]
-    history_data = stat_db[StatCollections.HISTORY_CURRENCY_PRICE_STAT].find(
-        {'_id': {'$gt': latest[0]}}, no_cursor_timeout=True).sort([('_id', ASCENDING)])
-    i = 0
-    print(latest)
-    for data in history_data:
-        t = data['_id']
-        i = i + 1
-        if i % 100 == 0:
-            print(t, i)
-
-        # 现在不需要使用 price_usd计价
-        # prices = data['price_usd']
-        # sort_price = sorted([[k, prices[k]] for k in list(prices.keys())], key=lambda x: x[1], reverse=True)
-        # for index, item in enumerate(sort_price):
-        #     stat_db[StatCollections.HISTORY_CURRENCY_RANK].find_one_and_update(
-        #         {'currency_id': item[0]},
-        #         {'$push': {'rank_price': [t, index, len(sort_price)]}},
-        #         upsert=True)
-
-        market_caps = data['market_cap']
-        sort_market_cap = sorted([[k, market_caps[k]] for k in list(market_caps.keys())], key=lambda x: x[1], reverse=True)
-        for index, item in enumerate(sort_market_cap):
-            stat_db[StatCollections.HISTORY_CURRENCY_RANK].find_one_and_update(
-                {'currency_id': item[0]},
-                {'$push': {'rank_market_cap': [t, index, len(sort_market_cap)]}},
-                upsert=True)
-
-
-def currency_rank_analyze2(ahead_time=0):
-    """计算每个货币排名随时间的变化"""
-    currencies = crawl_db[CrawlCollections.COINMARKET_CURRENCY_PRICE].find({'_id': '300-token'})
+def currency_rank_analyze(ahead_time=0):
+    """计算每个货币排名随时间的变化
+    :param ahead_time 单位精确到秒
+    """
+    currencies = crawl_db[CrawlCollections.COINMARKET_CURRENCY_PRICE].find({'_id': 'neo'})
     currency_ids = [c['id'] for c in currencies]
     start = time.time()
     for c_id in currency_ids:
         currency_rank = stat_db[StatCollections.HISTORY_CURRENCY_RANK].find_one({'currency_id': c_id})
+
+        # 计算最新分析的时间latest_time 精确到秒
         if currency_rank:
             market_cap_rank = currency_rank['rank_market_cap']
             sorted_rank = sorted(market_cap_rank, key=lambda x: x[0], reverse=True)
@@ -186,53 +220,32 @@ def currency_rank_analyze2(ahead_time=0):
         for history_data in histories:
             market_caps = history_data['market_cap']
             t = history_data['_id']
+
+            # 防止重复添加数据
             for index, item in enumerate(market_cap_rank):
                 if item[0] == t:
                     del market_cap_rank[index]
-            sorted_market_cap = sorted([[k, market_caps[k]] for k in market_caps.keys()], key=lambda x: x[1], reverse=True)
-            for index, item in enumerate(sorted_market_cap):
-                if item[0] == c_id:
-                    market_cap_rank.append([t, index, len(sorted_market_cap)])
-                    print('append', [timestamp_to_datetime(t), index, len(sorted_market_cap)])
-                    break
-            stat_db[StatCollections.HISTORY_CURRENCY_RANK].find_one_and_update(
-                {'currency_id': c_id},
-                {'$set': {"rank_market_cap": market_cap_rank}},
-                upsert=True)
+
+            # 将当天的市值排序
+            if c_id in market_caps:
+                df = pd.DataFrame([[k, market_caps[k]] for k in market_caps], columns=['name', 'values'])
+                df = df.sort_values(by='values', ascending=False)
+                count = len(df.index)
+                df['sort'] = range(count)
+                rank = int(df[df['name'] == c_id].iloc[0, 2])
+                market_cap_rank.append([t, rank, count])
+                print('append data {0} {1} {2}'.format(timestamp_to_datetime(t), rank, count))
+
+        stat_db[StatCollections.HISTORY_CURRENCY_RANK].find_one_and_update(
+            {'currency_id': c_id},
+            {'$set': {"rank_market_cap": market_cap_rank}},
+            upsert=True)
         print('analyze {0} completed, used {1}'.format(c_id, time.time() - start))
 
 
-def check_history_rank_data(c_id):
-    cs = stat_db[StatCollections.HISTORY_CURRENCY_RANK].find({'currency_id': c_id})
-    for item in cs:
-        rank = item['rank_market_cap']
-        check_rank(rank)
-        new_rank = []
-        times = []
-        for r in rank:
-            if r[0] not in times:
-                new_rank.append(r)
-                times.append(r[0])
-        print('after....')
-        check_rank(new_rank)
-        stat_db[StatCollections.HISTORY_CURRENCY_RANK].find_one_and_update(
-            {'_id': item['_id']},
-            {'$set': {'rank_market_cap': new_rank}})
-
-
-def check_rank(rank):
-    for index1, item1 in enumerate(rank):
-        count = 0
-        if item1[1] == 0:
-            print('error data', item1)
-        for index2, item2 in enumerate(rank):
-            if item1[0] == item2[0]:
-                count = count + 1
-        if count > 1:
-            print('dump data {0}---{1}'.format(count, item1[0]))
-
-
 if __name__ == '__main__':
-    # currency_rank_analyze2(ahead_time=24 * 3600 * 30)
-    # check_history_rank_data('300-token')
-    history_price_time_resample()
+    # currency_rank_analyze(ahead_time=24 * 3600 * 50)
+    # check_history_rank_data()
+    # history_price_time_resample(ahead_time=24 * 3600 * 50 * 1000)
+    market_cap_altcoin_resample(ahead_time=24 * 3600 * 50 * 1000)
+
